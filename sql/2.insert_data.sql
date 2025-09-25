08b434776dd33d82cd84418714b6e34ba6d373c3
Uwaberagiramata <141720280+Uwaberagiramata@users.noreply.github.com>
2025-09-25 17:05:12 +0200

    Update 2.insert_data.sql

--- sql/2.insert_data.sql
+++ sql/2.insert_data.sql
@@ -1,1 +1,11 @@
+1.CUSTOMERS
+--insert into customers
 
+INSERT INTO customers (customer_id, name, region) VALUES
+(1001, 'John Doe', 'Kigali'),
+(1002, 'Alice Umutoni', 'Nyarugenge'),
+(1003, 'Umutesi Dave', 'Huye'),
+(1004, 'Umuoza Aline', 'Kigali'),
+(1005, 'Mugabo Allan', 'Nyamagabe');
+
+3.PRODUCTS
