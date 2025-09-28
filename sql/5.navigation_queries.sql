0ba53a9cd53fa96d8ec2cadb507b678d32d1e3d2
Uwaberagiramata <141720280+Uwaberagiramata@users.noreply.github.com>
2025-09-28 18:41:24 +0200

    Update 5.navigation_queries.sql

--- sql/5.navigation_queries.sql
+++ sql/5.navigation_queries.sql
@@ -4,24 +4,34 @@
 
 -- Monthly sales with previous month (LAG) and next month (LEAD)
 
-
+--1.LEAD()
 SELECT
-    month,
-    total_sales,
-    LAG(total_sales, 1) OVER (ORDER BY month) AS prev_month_sales,
-    LEAD(total_sales, 1) OVER (ORDER BY month) AS next_month_sales,
+    DATE_TRUNC('month', sale_date) AS month,
+    SUM(amount) AS total_sales,
+    LEAD(SUM(amount)) OVER (ORDER BY DATE_TRUNC('month', sale_date)) AS next_month_sales,
     ROUND(
-        (total_sales - LAG(total_sales, 1) OVER (ORDER BY month)) 
-        / NULLIF(LAG(total_sales, 1) OVER (ORDER BY month), 0) * 100, 2
-    ) AS monthly_growth_percent
-FROM (
-    SELECT
-        DATE_TRUNC('month', sale_date) AS month,
-        SUM(amount) AS total_sales
-    FROM transactions
-    GROUP BY DATE_TRUNC('month', sale_date)
-) AS monthly
+        (LEAD(SUM(amount)) OVER (ORDER BY DATE_TRUNC('month', sale_date)) - SUM(amount)) 
+        / NULLIF(SUM(amount), 0) * 100, 2
+    ) AS growth_next_month_percent
+FROM transactions
+GROUP BY DATE_TRUNC('month', sale_date)
 ORDER BY month;
+
+--2.LAG()
+SELECT
+    DATE_TRUNC('month', sale_date) AS month,
+    SUM(amount) AS total_sales,
+    LAG(SUM(amount)) OVER (ORDER BY DATE_TRUNC('month', sale_date)) AS prev_month_sales,
+    ROUND(
+        (SUM(amount) - LAG(SUM(amount)) OVER (ORDER BY DATE_TRUNC('month', sale_date))) 
+        / NULLIF(LAG(SUM(amount)) OVER (ORDER BY DATE_TRUNC('month', sale_date)), 0) * 100, 2
+    ) AS monthly_growth_percent
+FROM transactions
+GROUP BY DATE_TRUNC('month', sale_date)
+ORDER BY month;
+
+
+
 --LAG: gives the previous month’s value(past)
 --LEAD: gives the next month’s value(future)
 
